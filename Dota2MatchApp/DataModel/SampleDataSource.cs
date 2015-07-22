5827788047f2643f278a6c9ffda7ba70f14a84ed
Brian Mc George <mcgbri004@myuct.ac.za>
2015-07-22 14:22:10 +0200

    Initial commit   - Rough but functionallity works   - Requires unit tests   - Requires refactor

---
+++ Dota2MatchApp/DataModel/SampleDataSource.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Storage;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Net.Http;
+
+// The data model defined by this file serves as a representative example of a strongly-typed
+// model.  The property names chosen coincide with data bindings in the standard item templates.
+//
+// Applications may use this model as a starting point and build on it, or discard it entirely and
+// replace it with something appropriate to their needs. If using this model, you might improve app 
+// responsiveness by initiating the data loading task in the code behind for App.xaml when the app 
+// is first launched.
+
+namespace Dota2MatchApp.Data
+{
+    /// <summary>
+    /// Generic item data model.
+    /// </summary>
+    public class SampleDataItem
+    {
+        public SampleDataItem(String id,
+                               String teamOneTag,
+                               String teamTwoTag,
+                               String teamOneScore,
+                               String teamTwoScore,
+                               String teamOneLogoUrl,
+                               String teamTwoLogoUrl,
+                               String league,
+                               String startTime,
+                               String seriesType
+                             )
+        {
+            this.UniqueId = id;
+            this.TeamOneTag = teamOneTag;
+            this.TeamTwoTag = teamTwoTag;
+            this.TeamOneScore = teamOneScore;
+            this.TeamTwoScore = teamTwoScore;
+            this.TeamOneLogoUrl = teamOneLogoUrl;
+            this.TeamTwoLogoUrl = teamTwoLogoUrl;
+            this.League = league;
+            this.StartTime = startTime;
+            this.SeriesType = seriesType;
+        }
+
+        public string UniqueId { get; private set; }
+        public string TeamOneTag { get; private set; }
+        public string TeamTwoTag { get; private set; }
+        public string TeamOneScore { get; private set; }
+        public string TeamTwoScore { get; private set; }
+        public string TeamOneLogoUrl { get; private set; }
+        public string TeamTwoLogoUrl { get; private set; }
+        public string League { get; private set; }
+        public string StartTime { get; private set; }
+        public string SeriesType { get; private set; }
+
+        public override string ToString()
+        {
+            return this.UniqueId;
+        }
+    }
+
+    /// <summary>
+    /// Generic group data model.
+    /// </summary>
+    public class SampleDataGroup
+    {
+        public SampleDataGroup(string uniqueId)
+        {
+            this.UniqueId = uniqueId;
+            this.Items = new ObservableCollection<SampleDataItem>();
+        }
+
+        public string UniqueId { get; private set; }
+        public ObservableCollection<SampleDataItem> Items { get; private set; }
+
+        public override string ToString()
+        {
+            return this.UniqueId.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Creates a collection of groups and items with content read from a static json file.
+    /// 
+    /// SampleDataSource initializes with data read from a static json file included in the 
+    /// project.  This provides sample data at both design-time and run-time.
+    /// </summary>
+    public sealed class SampleDataSource
+    {
+        private static SampleDataSource _sampleDataSource = new SampleDataSource();
+
+        private ObservableCollection<SampleDataGroup> _groups = new ObservableCollection<SampleDataGroup>();
+        public ObservableCollection<SampleDataGroup> Groups
+        {
+            get { return this._groups; }
+        }
+
+        public static async Task<IEnumerable<SampleDataGroup>> GetGroupsAsync()
+        {
+            await _sampleDataSource.GetSampleDataAsync();
+
+            return _sampleDataSource.Groups;
+        }
+
+        public static async Task<SampleDataGroup> GetGroupAsync(string uniqueId)
+        {
+            await _sampleDataSource.GetSampleDataAsync();
+            // Simple linear search is acceptable for small data sets
+            var matches = _sampleDataSource.Groups.Where((group) => group.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        public static async Task<SampleDataItem> GetItemAsync(string uniqueId)
+        {
+            await _sampleDataSource.GetSampleDataAsync();
+            // Simple linear search is acceptable for small data sets
+            var matches = _sampleDataSource.Groups.SelectMany(group => group.Items).Where((item) => item.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        private async Task GetSampleDataAsync()
+        {
+            if (this._groups.Count != 0)
+                return;
+            System.Diagnostics.Debug.WriteLine("It gets here 1");
+            Uri dataUri = new Uri("http://dailydota2.com/match-api");
+
+            //StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(dataUri);
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(dataUri);
+            var result = await response.Content.ReadAsStringAsync();
+            //string jsonText = await FileIO.ReadTextAsync(result);
+            System.Diagnostics.Debug.WriteLine(result.ToString());
+            System.Diagnostics.Debug.WriteLine(result);
+            JsonObject jsonObject = JsonObject.Parse(result);
+            System.Diagnostics.Debug.WriteLine("It gets here 2");
+            int timestamp = (int)jsonObject["timestamp"].GetNumber();
+            SampleDataGroup group = new SampleDataGroup("Group-1");
+            System.Diagnostics.Debug.WriteLine("It gets here 3");
+            foreach (JsonValue itemValue in jsonObject["matches"].GetArray())
+            {
+                System.Diagnostics.Debug.WriteLine("It gets here 4");
+                JsonObject itemObject = itemValue.GetObject();
+                System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+                dtDateTime = dtDateTime.AddSeconds(Convert.ToDouble(itemObject["starttime_unix"].GetString())).ToLocalTime();
+                System.Diagnostics.Debug.WriteLine(dtDateTime.ToString());
+                const string prefixUrl = "http://dailydota2.com";
+                group.Items.Add(new SampleDataItem(itemObject["link"].GetString(),
+                                                   itemObject["team1"].GetObject()["team_tag"].GetString(),
+                                                   itemObject["team2"].GetObject()["team_tag"].GetString(),
+                                                   itemObject["team1"].GetObject()["score"].GetString(),
+                                                   itemObject["team2"].GetObject()["score"].GetString(),
+                                                   prefixUrl + itemObject["team1"].GetObject()["logo_url"].GetString(),
+                                                   prefixUrl + itemObject["team2"].GetObject()["logo_url"].GetString(),
+                                                   "League: " + itemObject["league"].GetObject()["name"].GetString(),
+                                                   "Time: "+dtDateTime.ToString(),
+                                                   "Type: Best of " + ((int)itemObject["series_type"].GetNumber()).ToString()));
+            }
+            this.Groups.Add(group);
+            
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+#Dota 2 Pro match schedule
+##Windows Phone
+###Features
+  - Match schedule
+   - Teams
+   - Tournament
+   - Time
+   - Format
+  - Current match results
+
+###Future Features
+  - Recent results
+
+###To-do
+  - This is currently a quick and rough prototype. Which modifies the default app template. A cleanup and refactor of the code is required.
+  - Add unit tests for all current and future code (Template does not contain unit tests).
+
+###Special Thanks
+The app makes use of [Daily Dota 2's](http://dailydota2.com/) freely available [api](http://dailydota2.com/api).
